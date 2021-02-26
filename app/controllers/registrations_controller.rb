c4abce38fc0a28a235ba7017a960da700e5708d5
George <george.gbenle@outlook.com>
2021-02-26 19:24:37 +0000

    Add name and username functionality

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:member).permit(:name, :username, :email, :password, :password_confirmation)
+    params.require(:member).permit(:name, :membername, :email, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:member).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+    params.require(:member).permit(:name, :membername, :email, :password, :password_confirmation, :current_password)
   end
 end
