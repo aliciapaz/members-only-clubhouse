e09fd0964a59f35b563a16013346ceab22508310
George <george.gbenle@outlook.com>
2021-02-26 17:49:46 +0000

    Add controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,36 @@
 class PostsController < ApplicationController
+  
+  before_action :authenticate_member!, except: [:index, :show]
+
 
   def index
     @posts = Post.all.order("created_at DESC")
   end
   
   def new
-    @post = Post.new
+    @post =  current_member.posts.build
   end
 
   def create
-    
+    @post =  current_member.posts.build(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
   end
 
   def  show
     @post = Post.find(params[:id])
     @member = Member.find(@post.member_id)
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 
 end
