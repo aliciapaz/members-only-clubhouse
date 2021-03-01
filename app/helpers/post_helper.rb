f7cd7b413c4275ae8bee4d1197dfda5c14c6bd15
aliciapaz <aliciapazrojas@gmail.com>
2021-03-01 19:28:39 -0300

    fix rubocop offenses

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -4,7 +4,7 @@ module PostHelper
   end
 
   def main_post_author(posts)
-    if !posts.empty?
+    unless posts.empty?
       member_signed_in? ? "By #{Member.find(posts.last.member_id).name}" : 'By Anonymous member'
     end
   end
@@ -18,33 +18,34 @@ module PostHelper
   end
 
   def post_list_last(posts)
-    unless posts.empty?
-      link_to posts.last.title, posts.last
-    end
+    link_to posts.last.title, posts.last unless posts.empty?
   end
 
   def post_list_snd_last(posts)
-    if !posts.offset(2).limit(1).first.nil?
-      link_to posts.offset(2).limit(1).first.title, posts.offset(2).limit(1).first
+    unless posts.offset(2).limit(1).first.nil?
+      link_to posts.offset(2).limit(1).first.title,
+              posts.offset(2).limit(1).first
     end
   end
 
   def post_list_third_last(posts)
-    if !posts.offset(3).limit(1).first.nil?
-      link_to posts.offset(3).limit(1).first.title, posts.offset(3).limit(1).first
+    unless posts.offset(3).limit(1).first.nil?
+      link_to posts.offset(3).limit(1).first.title,
+              posts.offset(3).limit(1).first
     end
   end
 
   def post_list_fourth_last(posts)
-    if !posts.offset(4).limit(1).first.nil?
-      link_to posts.offset(4).limit(1).first.title, posts.offset(4).limit(1).first
+    unless posts.offset(4).limit(1).first.nil?
+      link_to posts.offset(4).limit(1).first.title,
+              posts.offset(4).limit(1).first
     end
   end
 
   def post_list_fifth_last(posts)
-    if !posts.offset(5).limit(1).first.nil?
-      link_to posts.offset(5).limit(1).first.title, posts.offset(5).limit(1).first
+    unless posts.offset(5).limit(1).first.nil?
+      link_to posts.offset(5).limit(1).first.title,
+              posts.offset(5).limit(1).first
     end
   end
-
 end
