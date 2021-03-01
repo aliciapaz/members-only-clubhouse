f6e0f4afcec4ced2937a2c8a76e73e8ebb466565
aliciapaz <aliciapazrojas@gmail.com>
2021-03-01 19:15:02 -0300

    remove logic from post_list

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -14,4 +14,35 @@ module PostHelper
   def post_author(member)
     member_signed_in? ? "By #{member.name}" : 'By Anonymous member'
   end
+
+  def post_list_last(posts)
+    unless posts.empty?
+      link_to posts.last.title, posts.last
+    end
+  end
+
+  def post_list_snd_last(posts)
+    if !posts.offset(2).limit(1).first.nil?
+      link_to posts.offset(2).limit(1).first.title, posts.offset(2).limit(1).first
+    end
+  end
+
+  def post_list_third_last(posts)
+    if !posts.offset(3).limit(1).first.nil?
+      link_to posts.offset(3).limit(1).first.title, posts.offset(3).limit(1).first
+    end
+  end
+
+  def post_list_fourth_last(posts)
+    if !posts.offset(4).limit(1).first.nil?
+      link_to posts.offset(4).limit(1).first.title, posts.offset(4).limit(1).first
+    end
+  end
+
+  def post_list_fifth_last(posts)
+    if !posts.offset(5).limit(1).first.nil?
+      link_to posts.offset(5).limit(1).first.title, posts.offset(5).limit(1).first
+    end
+  end
+
 end
